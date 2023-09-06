4bbc7b3df6342787ecb32c099485722f05cc5a76
CSEwkm <68175682+CSEwkm@users.noreply.github.com>
2023-09-06 21:59:22 +0800

    Update irfreader.cc

--- src/irfreader.cc
+++ src/irfreader.cc
@@ -2,21 +2,21 @@
 
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/Instructions.h"
-
+// IR 函数 reader
 IRFReader::IRFReader(Function* f) {
     this->f = f;
 }
 
 set<Instruction*> IRFReader::get_asserts() const {
     set<Instruction*> ret;
-
+    // function -> basicBlock
     for(BasicBlock& b : *(this->f)) {
-        for(Instruction& i : b) {
-            if(!isa<CallInst>(&i)) {
+        for(Instruction& i : b) {    // basicBlock -> Instruction
+            if(!isa<CallInst>(&i)) {    // Instruction不是CallInst 跳过
                 continue;
             }
 
-            CallInst* ci = dyn_cast<CallInst>(&i);
+            CallInst* ci = dyn_cast<CallInst>(&i);    // 强制类型转换 安全地向下转型
             Function* callee = ci->getCalledFunction();
             if(callee == nullptr) {
                 continue;
@@ -26,8 +26,8 @@ set<Instruction*> IRFReader::get_asserts() const {
             if(name != "abort" && name != "__assert_fail") {
                 continue;
             }
-
-            ret.insert(&i); 
+            // abort 或者 __assert_fail 函数
+            ret.insert(&i);     
         }
     }
 
